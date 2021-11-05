bef8e55d505b68b893b8ff58d4bf32417c6afe8e
techtheriac <jezreel.franklin@decagon.dev>
2021-11-05 14:02:31 +0100

    Initialize EfCore

---
+++ FlyingDutchmanAirlines/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace FlyingDutchmanAirlines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeHost();
+        }
+
+        private static void InitializeHost() =>        
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(builder => {
+                    builder.UseStartup<Startup>();
+                    builder.UseUrls("http://0.0.0.0:8000");
+                }).Build().Run();
+    }
+}

809cb4da30e826e0a60e9f30a05aba7f4617d10f
techtheriac <jezreel.franklin@decagon.dev>
2021-11-13 16:34:06 +0100

    Implement SaveChangesAsync using lambdas

--- FlyingDutchmanAirlines_Tests/Stubs/FlyingDutchmanAirlinesContext_Stub.cs
+++ FlyingDutchmanAirlines_Tests/Stubs/FlyingDutchmanAirlinesContext_Stub.cs
@@ -1,9 +1,11 @@
 ﻿using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.Exceptions;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FlyingDutchmanAirlines_Tests.Stubs
@@ -13,6 +15,18 @@ namespace FlyingDutchmanAirlines_Tests.Stubs
         public FlyingDutchmanAirlinesContext_Stub(DbContextOptions<FlyingDutchmanAirlinesContext> options) : base(options)
         {
 
+        }
+
+        public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            // Exceptions are devoid of type signatures
+            Func<Task<int>>[] functions =
+            {
+                () => throw new Exception("Database Error!"),
+                async () => await base.SaveChangesAsync(cancellationToken),
+            };
+
+            return await functions[(int)base.Bookings.First().CustomerId].Invoke();
         }
     }
 }
